e35fc022bcf7ed679de11b5893a88b8c22e6b980
Andreas Heimann <andreas@iklikla.de>
2019-03-26 16:51:12 +0100

    Update itdschungel.sql

--- itdschungel.sql
+++ itdschungel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 25. Mrz 2019 um 20:38
--- Server-Version: 10.1.36-MariaDB
--- PHP-Version: 7.2.11
+-- Erstellungszeit: 26. Mrz 2019 um 16:50
+-- Server-Version: 10.1.25-MariaDB
+-- PHP-Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -47,20 +47,23 @@ CREATE TABLE `course` (
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `short_description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `long_description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `difficulty` int(11) NOT NULL,
-  `location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `difficulty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Daten für Tabelle `course`
 --
 
-INSERT INTO `course` (`id`, `course_category`, `title`, `short_description`, `long_description`, `difficulty`, `location`) VALUES
-(1, 1, 'HTML-Grundkurs', 'Neben dem HTML Grundgerüst werden auch weitere Elemente der Syntax vorgestellt.', 'HTML bedeutet Hypertext Markup Language und wird genutzt, um Internetseiten mit Inhalt zu füllen. Wie genau das geht lernst Du in diesem Kurs. Dazu gehört natürlich auch, welche Regeln bei dem Aufbau und dem Aussehen von HTML-Elementen zu beachten sind, sprich der Syntax von HTML. ', 1, 'TODO'),
-(2, 1, 'HTML-Erweiterungen', 'Erstellen eines Seitenlayouts Mittels HTML und CSS.', 'Wenn Du die Grundlagen in HTML schon beherrscht, bist Du sicher erfreut, wenn die Webseite auch einem ästhetischen Anspruch gerecht wird. Dies kannst Du mit CSS erreichen. CSS steht für Cascading Style Sheets und ermöglicht das Erstellen von Layouts und zur individuellen Gestaltung von HTML-Elementen.', 3, 'TODO'),
-(3, 1, 'Responsives Webdesign', 'Erstellen einer responsive Webseite für verschiedene Geräte und Viewports.', '„Mit der Zeit gehen“ - Dies gilt besonders für das Webdesign. Dazu gehört heutzutage das Erstellen einer responsiven Website. Das ist erforderlich, wenn die Webseite für verschiedene Geräte optimiert sein soll, also für den Computer, das Tablet und auch für das Smartphone. Wie das funktioniert kannst Du in hier lernen.', 3, 'TODO'),
-(4, 1, 'PHP-Grundkurs', 'Lerne den Grundaufbau einer PHP-Anwendung sowie die Syntax.', 'Wenn Dir eine Webseite nur zum Betrachten nicht reicht, du auch Formulare nutzen möchtest um Informationen zu übermitteln, ist die Anwendung von PHP von Nöten. PHP steht für Hypertext Preprocessor, früher Personal Home Page Tools und ist eine serverseitige Skriptsprache. Die Grundlagen dessen kannst Du dir in diesem Kurs aneignen.', 1, 'TODO'),
-(5, 1, 'PHP-Erweiterungen', 'Erweiterte Kenntnisse über PHP wie Schleifen, Anweisungen und verschaltete Abfragen.', 'PHP kann vieles und wenn du die Grundlagen schon beherrscht, kannst Du hier lernen, wie man mit PHP Schleifen, Anweisungen und  verschaltete Abfragen erstellt.', 1, 'TODO');
+INSERT INTO `course` (`id`, `course_category`, `title`, `short_description`, `long_description`, `difficulty`) VALUES
+(1, 1, 'HTML-Grundkurs', 'Neben dem HTML Grundgerüst werden auch weitere Elemente der Syntax vorgestellt.', 'HTML bedeutet Hypertext Markup Language und wird genutzt, um Internetseiten mit Inhalt zu füllen. Wie genau das geht lernst Du in diesem Kurs. Dazu gehört natürlich auch, welche Regeln bei dem Aufbau und dem Aussehen von HTML-Elementen zu beachten sind, sprich der Syntax von HTML. ', 1),
+(2, 1, 'HTML-Erweiterungen', 'Erstellen eines Seitenlayouts Mittels HTML und CSS.', 'Wenn Du die Grundlagen in HTML schon beherrscht, bist Du sicher erfreut, wenn die Webseite auch einem ästhetischen Anspruch gerecht wird. Dies kannst Du mit CSS erreichen. CSS steht für Cascading Style Sheets und ermöglicht das Erstellen von Layouts und zur individuellen Gestaltung von HTML-Elementen.', 3),
+(3, 1, 'Responsives Webdesign', 'Erstellen einer responsive Webseite für verschiedene Geräte und Viewports.', '„Mit der Zeit gehen“ - Dies gilt besonders für das Webdesign. Dazu gehört heutzutage das Erstellen einer responsiven Website. Das ist erforderlich, wenn die Webseite für verschiedene Geräte optimiert sein soll, also für den Computer, das Tablet und auch für das Smartphone. Wie das funktioniert kannst Du in hier lernen.', 3),
+(4, 1, 'PHP-Grundkurs', 'Lerne den Grundaufbau einer PHP-Anwendung sowie die Syntax.', 'Wenn Dir eine Webseite nur zum Betrachten nicht reicht, du auch Formulare nutzen möchtest um Informationen zu übermitteln, ist die Anwendung von PHP von Nöten. PHP steht für Hypertext Preprocessor, früher Personal Home Page Tools und ist eine serverseitige Skriptsprache. Die Grundlagen dessen kannst Du dir in diesem Kurs aneignen.', 1),
+(5, 1, 'PHP-Erweiterungen', 'Erweiterte Kenntnisse über PHP wie Schleifen, Anweisungen und verschaltete Abfragen.', 'PHP kann vieles und wenn du die Grundlagen schon beherrscht, kannst Du hier lernen, wie man mit PHP Schleifen, Anweisungen und  verschaltete Abfragen erstellt.', 1),
+(6, 2, 'Grundkurs C++', 'Einführung in C++ mit einfachen Erweiterungen bei Nutzung von Ein- und Ausgabestreams', 'Um eigene Programme zu erstellen kannst Du C++ nutzen. Eine Programmiersprache für die Systemprogrammierung und der Anwendungsprogrammierung.', 2),
+(7, 2, 'C++ für Fortgeschrittene', 'Pointer, Templates und OOP. Dieser Kurs vermittelt fortgeschrittene Inhalte zu C++.', 'Mehr C++ für die erfahreneren Programmierer. Hier geht es um fortgeschrittenere Inhalte zu C++, wie beispielsweise die Erstellung von Pointer, Templates und das objektorientierte Programmieren.', 3),
+(8, 2, 'Einstieg in die Programmierung', 'Bekomme einen Überblick über den Software-Entwicklungsprozess und die Planung von Programmen.', 'Wenn Du dich für das Programmieren interessierst und noch sehr unerfahren in diesem Bereich bist, dann ist der Einstiegskurs zur Programmierung von uns genau das richtige für dich. Hier bekommst Du einen Überblick über denn Software Entwicklungsprozess und die Planung von Programmen.', 1),
+(9, 2, 'XML-Programmierung', 'Lerne, wie du durch XML deine Datenweitergabe optimieren kannst.', 'In diesem Kurs lernst du die Grundlagen von XML und wie man XML in die Datenwiedergabe einbinden kann, um Arbeitsabläufe zu optimieren.', 1);
 
 -- --------------------------------------------------------
 
@@ -98,10 +101,24 @@ CREATE TABLE `coursedate` (
 --
 
 INSERT INTO `coursedate` (`id`, `course_date`, `course`) VALUES
-(1, '2019-03-07', 1),
-(2, '2019-03-08', 1),
-(3, '2019-03-14', 4),
-(4, '2019-03-15', 4);
+(5, '2019-04-27', 1),
+(6, '2019-04-28', 1),
+(7, '2019-05-11', 2),
+(8, '2019-05-12', 2),
+(9, '2019-05-24', 3),
+(10, '2019-05-25', 3),
+(11, '2019-06-01', 4),
+(12, '2019-06-02', 4),
+(13, '2019-06-15', 5),
+(14, '2019-06-16', 5),
+(15, '2019-04-27', 6),
+(16, '2019-04-28', 6),
+(17, '2019-05-11', 7),
+(18, '2019-05-12', 7),
+(19, '2019-04-27', 8),
+(20, '2019-04-28', 8),
+(21, '2019-06-01', 9),
+(22, '2019-06-02', 9);
 
 -- --------------------------------------------------------
 
@@ -127,8 +144,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `forename`, `lastname`, `adress`, `zip`, `city`, `security_key`) VALUES
-(1, 'ahe', '559e642972ad12ff5b16cd71f1e2b50aa95a522c', 'andreas@iklikla.de', 'Andreas', 'Heimann', 'Kranichstraße22 ', 46282, 'Dorsten', '5c992d1100c6c'),
-(2, 'hansi', 'ddd7c80da5a004e7abb70e30498ab1b8d9e6da16', 'tolle@email.de', 'Hans', 'Franz', NULL, NULL, NULL, '5c6d436e95c66');
+(1, 'ahe', '559e642972ad12ff5b16cd71f1e2b50aa95a522c', 'privat@andreas-heimann.com', 'Max', 'Heimann', 'Kranichstraße 22', 46282, 'Dorsten', '5c9a48386bbdf'),
+(2, 'hansi', 'franz', 'tolle@email.de', 'Hans', 'Franz', NULL, NULL, NULL, '5c6d436e95c66');
 
 --
 -- Indizes der exportierten Tabellen
@@ -177,32 +194,27 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `bookedcourse`
 --
 ALTER TABLE `bookedcourse`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT für Tabelle `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT für Tabelle `coursecategory`
 --
 ALTER TABLE `coursecategory`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT für Tabelle `coursedate`
 --
 ALTER TABLE `coursedate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- Constraints der exportierten Tabellen
 --
