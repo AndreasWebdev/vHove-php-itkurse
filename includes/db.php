80241be9a764fe2a738c3a7643625250adb4bf54
Andreas Heimann <andreas@iklikla.de>
2019-03-26 17:03:05 +0100

    less strict errors

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,18 @@
 <?php
 	error_reporting(E_ALL & ~E_NOTICE);
-	// mysqli_report(MYSQLI_REPORT_ALL);
+	//mysqli_report(MYSQLI_REPORT_STRICT);
 
     define('MYSQL_HOST', 'localhost');
     define('MYSQL_DB', 'itdschungel');
     define('MYSQL_USER', 'root');
     define('MYSQL_PASS', '');
 
-    $db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
-    $db->set_charset("utf8");
+	try {
+		$db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
+		$db->set_charset("utf8");
+	} catch(Exception $ex) {
+		echo "<h1>Datenbankverbindung schlug fehl!</h1>";
+		var_dump($ex);
+		exit;
+	}
 ?>
